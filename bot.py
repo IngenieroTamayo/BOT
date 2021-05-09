de18889b5f61414004d6542becdfebc31e1f2edb
Juan Tamayo <juantamayo203@gmail.com>
2021-05-09 15:15:26 -0500

    Generación de API mediante numero y mensaje

---
+++ bot.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+import time
+import os, time
+
+driver = webdriver.Chrome(executable_path=r"C:\driverchrome\chromedriver.exe")
+
+driver.get("https://web.whatsapp.com/")
+time.sleep(5)
+
+celular = "573122787169"
+mensaje = "Hola, mi primer bot"
+
+driver.get("https://wa.me/"+celular+"?text="+mensaje)
+time.sleep(5)
+
+btn = driver.find_elements_by_xpath("//*[@id='action-button']")[0]
+btn.click()
+time.sleep(5)
+
+btn = driver.find_element_by_xpath("//*[@id='fallback_block']/div/div/a")
+btn.click()
+time.sleep(5)
+
+driver.close()
